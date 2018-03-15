05a9e1b1bdeacfb2233918601ea6e98530413124
xt <118093396@qq.com>
2018-03-15 17:37:09 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <template>
-<div id="app">
+<div id="app" style="min-width: 1200px;">
   <div class="menu-wrapper">
     <div class="menu-block">
-      <p data-type='title-name'>
-        <Icon type="earth" />
-        智能知识库
-      </p>
-      <p class='menu-item-wrapper'>
-        <Menu mode="horizontal" :theme="theme" active-name="1">
+      <p class='menu-item-wrapper' style="background-color: #fff">
+        <Menu mode="horizontal" :theme="theme" active-name="1" style="max-width: 1200px; margin: auto">
+          <MenuItem name="1" class='menu-title'>
+              <Icon type="earth"></Icon>
+              智能知识库
+          </MenuItem>
           <MenuItem name="1">
               <Icon type="ios-paper"></Icon>
               解决方案
           </MenuItem>
           <MenuItem name="2">
-              <Icon type="ios-people"></Icon>
+              <Icon type="android-search"></Icon>
               知识搜索
           </MenuItem>
           <MenuItem name="3">
-              <Icon type="ios-people"></Icon>
+              <Icon type="bookmark"></Icon>
               知识图谱
           </MenuItem>
           <MenuItem name="4">
-              <Icon type="settings"></Icon>
+              <Icon type="android-star"></Icon>
               我的偏好
           </MenuItem>
       </Menu>
       </p>
+      <div style="max-width: 1200px; margin: auto; padding: 24px">
+        <div class="title-info">
+          <p style="font-size: 36px;">物联网 ---智慧城市解决方案</p>
+          <p style="font-size: 20px;">集“物联、数联、智联”三位一体的驱动模式 ，构建新型智慧城市架构</p>
+        </div>
+      </div>
     </div>
-
+  </div>
+  <div class="tab-menu-wrapper">
+    <div data-type='item' data-active='active'>
+      <Icon type="social-buffer" style='font-size: 42px; padding-bottom: 12px;'></Icon>
+      <p>通用架构解读</p>
+    </div>
+    <div data-type='item'>
+      <Icon type="earth" style='font-size: 42px; padding-bottom: 12px;'></Icon>
+      <p>经典应用场景</p>
+    </div>
+    <div data-type='item'>
+      <Icon type="ios-book" style='font-size: 42px; padding-bottom: 12px;'></Icon>
+      <p>技术解决方案</p>
+    </div>
   </div>
   <router-view></router-view>
 </div>
@@ -45,12 +64,18 @@
 <style lang='less' scoped>
 .menu-wrapper {
   position: relative;
-  
+  height: 400px;
+  background-size: cover;
+  background-image: url('http://p5jbmbfmc.bkt.clouddn.com/smartcity.jpg');
 }
 .menu-block {
   & [data-type='title-name'] {
     font-size: 18px;
   }
+}
+.menu-title {
+  font-size: 20px;
+  padding-right: 60px;
 }
 .menu-item-wrapper {
   & .ivu-menu:after {
@@ -58,6 +83,33 @@
   }
   & .ivu-menu-item {
     border-bottom: none !important;
+  }
+}
+.title-info {
+  padding-top: 80px;
+  color: white;
+  font-weight: bold;
+}
+.tab-menu-wrapper {
+  max-width: 1400px;
+  padding: 24px;
+  margin: auto;
+  &:after {
+    content: '';
+    clear: both;
+    display: table;
+  }
+  & [data-type='item'] {
+    float: left;
+    width: 33.33%;
+    text-align: center;
+    font-size: 20px;
+    &:nth-last-of-type(n + 2) {
+      border-right: 1px solid #666;
+    }
+  }
+  & [data-active='active'] {
+    color: #2d8cf0
   }
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,152 @@
 <template>
-  <div class="">
-    text
+  <div class="" style="background-color: rgb(243, 243, 243); position: relative;">
+
+      <div class="content-wrapper" >
+        <div class="content-img">
+          <div class="img-select-wrappr">
+            <ButtonGroup>
+               <Button size="large" type="primary">物联网+城市管理</Button>
+               <Button size="large" >小区进出影像采集</Button>
+               <Button size="large">小区消防通道地磁</Button>
+           </ButtonGroup>
+         </div>
+         <template v-if='tabname == 1'>
+           <img src="http://p5jbmbfmc.bkt.clouddn.com/img1.png" style="max-width: 100%; max-height: 100%" alt="">
+         </template>
+        </div>
+        <div class="content-text">
+          <div class="content-text-block">
+            <template v-if='tabname == 1'>
+               <h2>
+                 现状需求
+               </h2>
+             <Form :label-width="40">
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                   小区入口传统监控系统技术落后、智能化水平低、资源利用率不足、维护量大。
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 大量流动人口涌入与平安城市监控管理之间的矛盾更加凸显。
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 缺乏对往来人员的记录和监督，导致事件发生后，难以取证。
+               </FormItem>
+             </Form>
+
+               <h2 style='padding-top: 24px'>
+                 解决方案
+               </h2>
+             <Form :label-width="40">
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 人员车辆布控预警
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 动态人脸比对
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 轨迹查询
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 预案搜索
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 目标特征检索
+               </FormItem>
+               <FormItem>
+                 <template slot='label'>
+                   <Icon type="ios-circle-filled"></Icon>
+                 </template>
+                 以图搜图
+               </FormItem>
+             </Form>
+
+            </template>
+          </div>
+        </div>
+      </div>
+
   </div>
+
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      tabname: 1
+    }
+  }
 }
 </script>
 
 
-<style scoped>
+<style lang='less' scoped>
+.content-wrapper {
+  margin: auto;
+  max-width: 1400px;
+  height: 840px;
+  &:after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+}
+.content-img {
+  width: 66.2%;
+  float: left;
+  height: 100%;
+  padding: 96px 32px 32px ;
+  text-align: center;
+  position: relative;
+}
+.content-text {
+  width: 33.8%;
+  float: left;
+  height: 100%;
+  border-left: 1px dashed rgba(73, 198, 251, 0.69);
+  position: relative;
+}
+.img-select-wrappr {
+  position: absolute;
+  top: 24px;
+}
+.content-text-block {
+  position: relative;
+  padding: 24px 12px;
+  h2 {
+    padding-bottom: 10px;
+    color: #2d8cf0;
+    border-bottom: 1px solid #2d8cf0;
+  }
 
+  :global(.ivu-form-item-content) {
+    font-size: 15px !important;
+  }
+  :global(.ivu-form-item) {
+    margin-bottom: 12px;
+  }
+}
 </style>
